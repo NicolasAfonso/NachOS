833871e7671fc5892427944c250523250e986887
pierre <fake@thegame.net>
2013-02-21 17:14:51 +0100

    modification du programme de test : code/test/putchar.c

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -6,31 +6,34 @@
 
 
 void print(char c, int n) {
-        //int i;
+        int i;
         char tab[n];
-/*
+
         for (i = 0; i < n; i++) {
                 //PutChar(c+i);
                 tab[i]=c+i;
           
         }
-
-        //PutChar('\n');
         tab[n]='\0';
         PutString(tab);
-*/
+
+        PutChar('\n');
+}
+
+void printString(int n) {
 //pour tester get et put string
+        char tab[n];
         PutString("tapez un texte:");
         GetString(tab,n);
         PutString("vous avez ecrit :");
         PutString(tab);
-        
 }
 int
 main()
 {
-  print('a',26);
-  Halt();
+        printString(50);
+        //print('a',26);
+        Halt();
 }
 
 
